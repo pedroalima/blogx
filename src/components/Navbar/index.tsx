9134be030e8a618963ea3ed4bcb9305c43986fd5
Pedro Lima <tsc.pedrolima@gmail.com>
2023-08-18 16:27:56 -0300

    style: add buttons navbar

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,10 +1,21 @@
-
+import { BottomNavigation, BottomNavigationAction } from "@mui/material";
+import HomeIcon from "@mui/icons-material/Home";
+import PostAddIcon from "@mui/icons-material/PostAdd";
+import { Link } from "react-router-dom";
+import { NavWrapper } from "./style";
 
 function Navbar() {
 	return (
-		<nav>
-			<h1>BlogX</h1>
-		</nav>
+		<NavWrapper>
+			<BottomNavigation showLabels>
+				<Link to="/">
+					<BottomNavigationAction label="Home" icon={<HomeIcon />} />
+				</Link>
+				<Link to="/new">
+					<BottomNavigationAction label="NewPost" icon={<PostAddIcon />} />
+				</Link>
+			</BottomNavigation>
+		</NavWrapper>
 	);
 }
 
